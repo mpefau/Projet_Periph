e04b215b05e71f1e567629962b9a1e56c50c47db
Paul Meis <32903531+goowza@users.noreply.github.com>
2018-11-06 15:03:30 -0500

    Add files via upload
    Ajout fonctions d'initialisation

--- nucleo_base_cube_mx/Drivers/MyDrivers/InfraRouge.c
+++ nucleo_base_cube_mx/Drivers/MyDrivers/InfraRouge.c
@@ -1,1 +1,36 @@
 #include "InfraRouge.h"
+
+// renvoi un ADC_HandleTypeDef de ADC1
+ADC_HandleTypeDef newADC() {
+	
+	ADC_HandleTypeDef new_adc;
+	
+	// Configuration de l'ADC
+	__HAL_RCC_ADC_CONFIG(RCC_ADCPCLK2_DIV6);
+	__ADC1_CLK_ENABLE();
+	
+	// Création d'une structure d'initialisation de ADC
+	ADC_InitTypeDef initADC;
+	initADC.DataAlign = ADC_DATAALIGN_RIGHT; // MSB sur le bit 11, LSB sur bit 0
+	initADC.ScanConvMode = ADC_SCAN_DISABLE; // On est en mode single channel donc on lit juste le premier
+	initADC.ContinuousConvMode = DISABLE; // Mode continuous desactivé
+	initADC.NbrOfConversion = 1; // Nombre de channels à lire 
+	initADC.DiscontinuousConvMode = ENABLE; // Mode discontinu activé
+	initADC.NbrOfDiscConversion = 1;
+	initADC.ExternalTrigConv = ADC_SOFTWARE_START; // ADC déclenché par le programme
+	
+	new_adc.Instance = ADC1;
+	new_adc.Init = initADC;
+	
+	return new_adc;
+}
+
+// renvoi un channel 1 avec les bonnes valeurs
+ADC_ChannelConfTypeDef newChan(uint32_t channel, uint32_t rank, uint32_t sampletime) {
+	ADC_ChannelConfTypeDef new_chan;
+	new_chan.Channel = channel;
+	new_chan.Rank = rank;
+	new_chan.SamplingTime = sampletime; // Nombre de cycle d'horloge par conversion
+	
+	return new_chan;
+}
--- nucleo_base_cube_mx/Drivers/MyDrivers/InfraRouge.h
+++ nucleo_base_cube_mx/Drivers/MyDrivers/InfraRouge.h
@@ -3,4 +3,10 @@
 
 #include "stm32f1xx_hal.h"
 
+// renvoi un ADC_HandleTypeDef de ADC1
+ADC_HandleTypeDef newADC();
+
+// renvoi un channel 1 avec les bonnes valeurs
+ADC_ChannelConfTypeDef newChan(uint32_t channel, uint32_t rank, uint32_t sample_time);
+
 #endif
